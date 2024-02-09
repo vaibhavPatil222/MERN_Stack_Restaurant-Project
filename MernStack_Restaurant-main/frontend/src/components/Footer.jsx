5c99756c68b1db5ff32491417ace7c5021440718
vaibhavPatil222 <ptanmay>
2024-02-09 23:03:10 +0530

    64 files uploads

---
+++ MernStack_Restaurant-main/frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">VAIBHAV PATIL</div>
+          <div className="right">
+            <p>B-17, Bhumkar Chowk, Pimpari Chinchwad</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By VAIBHAV PATIL</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Vaibhav.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
